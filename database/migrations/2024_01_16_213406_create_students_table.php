450825d292102a794fb52c98f1e6597661349c56
VOOLDZ <aymangh854@gmail.com>
2024-01-17 00:36:12 +0300

    Create student model and migration

---
+++ database/migrations/2024_01_16_213406_create_students_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->date('birthday')->nullable(); // YYYY-M-D
+            $table->string('phone_number', 15)->nullable();
+            $table->integer('attendance')->default(0);
+            $table->string('location')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
