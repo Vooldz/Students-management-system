d66da7ab6acabb32acdc8e6cf403e583e95865a6
VOOLDZ <aymangh854@gmail.com>
2024-01-17 02:59:24 +0300

    Create the check studentce functrionality

--- app/Http/Controllers/Api/V1/Students/GetStudentController.php
+++ app/Http/Controllers/Api/V1/Students/GetStudentController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\V1\Students\GetStudentResource;
 
+
 class GetStudentController extends Controller
 {
     public function index()
